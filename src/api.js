faab0d6296f7b91edc8125688ec3a0ca48550ab9
rkshaown <rkshawon36@gamil.com>
2023-08-12 21:47:33 +0600

    updated

--- src/api.js
+++ src/api.js
@@ -1,19 +1,43 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
-const api = async (input) => {
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
+//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
 
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    messages: input,
-    max_tokens: 100,
-    temperature: 0,
-  });
+// const api = async (input) => {
+//   const configuration = new Configuration({
+//     apiKey: "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",
+//   });
 
-  return response.data.choices[0].text.replace(/\n/g, "");
+//   const openai = new OpenAIApi(configuration);
+//   const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     messages: input,
+//     max_tokens: 100,
+//     temperature: 0,
+//   });
+
+//   return response.data.choices[0].text.replace(/\n/g, "");
+// };
+import axios from "axios";
+
+export const api = async (chat) => {
+  try {
+    // after deployment, you should change the URL below
+    const response = await axios.post(
+      "http://localhost:8000",
+      {
+        message: chat,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const featuredQuestion = [
